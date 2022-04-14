4814d068339cc77f6e62f93510d87aa16825ac6f
Russell Fraze <russell.fraze@gmail.com>
2022-04-14 13:30:48 -0700

    forgot password functionality

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -1,7 +1,61 @@
+import {getAuth, sendPasswordResetEmail} from 'firebase/auth'
+import {useState} from 'react'
+import {Link} from 'react-router-dom'
+import {toast} from 'react-toastify'
+import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
+
 function ForgotPasswrod() {
+    const [email, setEmail] = useState('')
+
+    const onChange = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const onSubmit = async(e) => {
+        e.preventDefault()
+
+        try {
+            const auth = getAuth()
+            await sendPasswordResetEmail(auth, email)
+            toast.success('Email was sent')
+        } catch (error) {
+            toast.error('could not send reset email')
+        }
+    }
+
+
     return (
-        <div>
-            <h1>Forgot Passwrod</h1>
+        <div className='pageContainer'>
+            <header className='pageHeader'>
+                <p className='pageHeader'>Forgot Password</p>
+            </header>
+
+            <main>
+                <form onSubmit={onSubmit}>
+                    <input 
+                    className='emailInput'
+                    type='email' 
+                    placeholder='email' 
+                    id='email' 
+                    value={email} 
+                    onChange={onChange} 
+                    />
+                    <Link className="forgotPasswordLink" to='/sign-in'>
+                        Sign In
+                    </Link>
+
+                    <div className='signInBar'>
+                        <div className='signInText'>
+                            Send Reset Link
+                        </div>
+                        <button className='signInButton'>
+                            <ArrowRightIcon fill='#ffffff' width='34px' height='34px'/>
+                        </button>
+                    </div>
+
+
+                </form>
+            </main>
         </div>
     )
 }
