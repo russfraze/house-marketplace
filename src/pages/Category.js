b7e3764617b85cffbf78cae869ecd2fdc135506e
Russell Fraze <russell.fraze@gmail.com>
2022-04-19 14:19:24 -0700

    listing item component

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,9 +1,10 @@
-import {useEffect, useState} from 'react'
-import {useParams} from 'react-router-dom'
-import {collection, getDocs, query, where, orderBy, limit, startAfter} from 'firebase/firestore'
-import {db} from '../firebase.config'
-import {toast} from 'react-toastify'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { collection, getDocs, query, where, orderBy, limit, startAfter } from 'firebase/firestore'
+import { db } from '../firebase.config'
+import { toast } from 'react-toastify'
 import Spinner from '../components/Spinner'
+import ListingItem from '../components/ListingItem'
 
 function Category() {
 
@@ -21,7 +22,7 @@ function Category() {
                 //create a query 
                 //the params is whats linked to the path in the route in app.js
                 const q = query(listingsRef, where('type', '==', params.categoryName),
-                orderBy('timestamp', 'desc'),limit(10)
+                    orderBy('timestamp', 'desc'), limit(10)
                 )
 
                 //Exicute query
@@ -44,30 +45,34 @@ function Category() {
         }
 
         fetchListings()
-    },[params.categoryName])
+    }, [params.categoryName])
 
 
     return (
         <div className="category">
             <header>
                 <p className="pageHeader">
-                    {params.categoryName === 'rent' 
-                    ? 'Places for rent' 
-                    : 'Places for sale'}
+                    {params.categoryName === 'rent'
+                        ? 'Places for rent'
+                        : 'Places for sale'}
                 </p>
             </header>
 
-            {loading ? <Spinner /> : listings && listings.length > 0 ? 
-            <>
-            <main>
-                <ul className="categoryListings">
-                    {listings.map((listing) => (
-                        <h3>{listing.data.name}</h3>
-                    ))}
-                </ul>
-            </main>  
-            </> 
-            : <p>No listings for {params.categoryName}</p>}
+            {loading ? <Spinner /> : listings && listings.length > 0 ?
+                <>
+                    <main>
+                        <ul className="categoryListings">
+                            {listings.map((listing) => (
+                                <ListingItem
+                                    listing={listing.data}
+                                    id={listing.id}
+                                    key={listing.id}
+                                />
+                            ))}
+                        </ul>
+                    </main>
+                </>
+                : <p>No listings for {params.categoryName}</p>}
         </div>
     )
 }
