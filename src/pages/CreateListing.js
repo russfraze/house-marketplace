0eb83fd51b7c95f79c1a0aba0664c0737e1ba17b
Russell Fraze <russell.fraze@gmail.com>
2022-04-24 18:26:46 -0700

    uploading images and save listings

--- src/pages/CreateListing.js
+++ src/pages/CreateListing.js
@@ -1,10 +1,15 @@
 import { useState, useEffect, useRef } from 'react'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage"
+import {addDoc, collection, serverTimestamp} from 'firebase/firestore'
+import { db } from '../firebase.config'
 import { useNavigate } from 'react-router-dom'
 import Spinner from '../components/Spinner'
+import { toast } from 'react-toastify'
+import { v4 as uuidv4 } from 'uuid'
 
 function CreateListing() {
-    const [geolocationEnabled, setGeolocationEnabled] = useState(true)
+    const [geolocationEnabled, setGeolocationEnabled] = useState(false)
     const [loading, setLoading] = useState(false)
     const [formData, setFormData] = useState({
         type: 'rent',
@@ -59,13 +64,106 @@ function CreateListing() {
         }
     }, [isMounted])
 
-    const onSubmit = (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault()
-        console.log(formData)
+        setLoading(true)
+        if (discountedPrice >= regularPrice) {
+            setLoading(false)
+            toast.error("Discounted price needs to be less than regular price")
+            return
+        }
+
+        if (images.length > 6) {
+            setLoading(false)
+            toast.error('Max 6 images')
+            return
+        }
+
+        let geolocation = {}
+        let location
+
+        if (geolocationEnabled) {
+            const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`)
+
+            const data = await response.json()
+
+            //there is other code here if your using geoLoaction from google later
+
+            console.log(data)
+        } else {
+            geolocation.lat = latitude
+            geolocation.lng = longitude
+            location = address
+            console.log(geolocation, location)
+        }
+
+        //Store images in firebase
+        const storeImage = async (image) => {
+            return new Promise((resolve, reject) => {
+                const storage = getStorage()
+                const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+
+                const storageRef = ref(storage, 'images/' + fileName)
+
+                const uploadTask = uploadBytesResumable(storageRef, image)
+
+                uploadTask.on('state_changed',
+                    (snapshot) => {
+                        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                        console.log('Upload is ' + progress + '% done');
+                        switch (snapshot.state) {
+                            case 'paused':
+                                console.log('Upload is paused');
+                                break;
+                            case 'running':
+                                console.log('Upload is running');
+                                break;
+                        }
+                    },
+                    (error) => {
+                        reject(error)
+                    },
+                    () => {
+                        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                            resolve(downloadURL);
+                        });
+                    }
+                );
+            })
+        }
+
+        const imgUrls = await Promise.all(
+            [...images].map((image) => storeImage(image))
+          ).catch(() => {
+            setLoading(false)
+            toast.error('Images not uploaded')
+            return
+          })
+
+        console.log(imgUrls)
+        const formDataCopy = {
+            ...formData,
+            imgUrls,
+            geolocation, 
+            timestamp: serverTimestamp()
+
+        }
+
+        delete formDataCopy.images
+        delete formDataCopy.address
+        location && (formDataCopy.location = location)
+        !formDataCopy.offer && delete formDataCopy.discountedPrice
+
+        const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
+
+        setLoading(false)
+        toast.success('Listing saved')
+        navigate(`/category/${formDataCopy.type}/${docRef.id}`)
+
     }
     const onMutate = (e) => {
         let boolean = null
-        
+
         if (e.target.value === 'true') {
             boolean = true
         }
@@ -82,7 +180,7 @@ function CreateListing() {
         }
 
         //Text/Booleans/Numbers 
-        if(!e.target.files){
+        if (!e.target.files) {
             setFormData((prevState) => ({
                 ...prevState,
                 [e.target.id]: boolean ?? e.target.value
@@ -103,7 +201,7 @@ function CreateListing() {
             <main>
                 <form onSubmit={onSubmit}>
                     <label className='formLabel'>Sell / Rent</label>
-                    <div class="formButtons">
+                    <div className="formButtons">
                         <button
                             type='button'
                             className={type === 'sale' ? 'formButtonActive' : 'formButton'}
